2a020f69f466be3b1a90b25d4efc244df444b64b
B221881-2022 <anon@anon.com>
2022-10-22 00:28:29 +0100

    Initial multicov on all BAM files

--- script4.sh
+++ script4.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
-
 bam_files=$(ls ./ICA1/fastq/*.sorted.bam)
 
 for bam in ${bam_files};
 	do
-	bedtools bamtobed -i ${bam}| bedtools intersect -a - -b ./ICA1/TriTrypDB-46_TcongolenseIL3000_2019.bed > "${bam}".intersect
-
+	bedtools multicov -bams ${bam} -bed ./ICA1/TriTrypDB-46_TcongolenseIL3000_2019.bed > "${bam}".multicov
 done
+
